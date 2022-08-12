d6b7ab414fcf5ca2d4fa8bc6674d6fd43c0ea865
OlegSam2 <doorsmania@mail.ru>
2022-08-12 18:44:48 +0300

    homework task10 the second digit of a three-digit number

---
+++ task10/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число 	
+// и на выходе показывает вторую цифру этого числа.	
+// 456 -> 5	
+// 782 -> 8	
+// 918 -> 1	
+
+Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int secondfigure = (number / 10) % 10;
+
+if (number >= 100 & number < 1000)
+{
+    Console.Write($"Вторая цифра {secondfigure} трехзначного числа {number}");
+}
+else
+{
+    Console.WriteLine("Введенное число не трехзначное");
+}
+
